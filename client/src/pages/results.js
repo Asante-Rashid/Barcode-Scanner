cc3e62fbc69626213788ad712ccf700e9b0e0412
jaclay1994 <jaclay94@gmail.com>
2021-05-22 19:33:26 -0400

    Adding functions into Scanner and results to render barcode and api calls.

--- client/src/pages/results.js
+++ client/src/pages/results.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Column from "../components/Column";
 import Card from "../components/Card";
 import Groupbtn from "../components/Groupbtn";
+import API from "../utils/API"
 
 
 function Results() {
+
+    const [scan, setScan] = useState([])
+    const [formObject, setFormObject] = useState({})
+
+    function loadScan() {
+        API.getBarcode()
+        .then(res => setScan(res.data))
+        .catch(err => console.log(err))
+    };
+
     return (
         <div>
             <Container>
